939fe8e860bc1bfe503a89e631fbbdfa2b0958d3
liumouyan <2387498813@qq.com>
2019-12-19 13:29:40 +0800

    首次提交

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+let a:string="123";
+const b:number=123;
+console.log(a)
+
+let gender:"男"|"女";
+gender="男";
+console.log(gender)
+
+let arr:[number,string];
+arr=[21,"fdf"]
+type login={
+    user:string,
+    password:number
+}
+function Login(key:login){
+    
+}
