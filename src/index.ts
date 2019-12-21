2caaacadca414626887ca2dc495e97c98bc6d73f
liumouyan <2387498813@qq.com>
2019-12-21 09:48:23 +0800

    类测试

--- src/index.ts
+++ src/index.ts
@@ -1,58 +1,11 @@
-
-
-interface handsome{
-    height:number,
-    face:string
-}
-interface User extends handsome{    //接口继承
-    readonly name:String,
-    age:number,
-    intellect:func,
-    readonly favorite:readonly string  []       //不能修改数组的引用以及修改数组的值
-}
-
-interface func{
-    (age:number):number,
-}
-
-const A:User={      //必须实现接口的所有属性包括继承的接口
-    name:"lige",
-    age:100,
-    intellect:(age)=>{console.log(this,age); return age+100},
-    favorite:["movie","music","basketball"],
-    height:180,
-    face:"beauty"
-}
-// A.favorite.push()
-console.log(A.intellect.call(A,A.age))
-
-
-/**
- * 鸭子类型检测
- */
-interface Dark{
-    weigth:number,
-    sing:()=>void,
-    sound:"嘎嘎嘎"
-}
-const person={
-    weigth:100,
-    sing:()=>{console.log("sing")},
-    height:180,
-    sound:"嘎嘎嘎" as "嘎嘎嘎"  //类型断言
-}
-const dark:Dark=person;
-console.log(dark.sing())
-
-const arr=[1,2,3,4,56];
-interface cb{
-    (n:number):void
-}
-const callback:cb=(n)=>{
-    if(n%2==0){
-        console.log("偶数")
+class User{
+    name:string
+    readonly age:number
+    private id:number
+    constructor(name:string,age:number,id:number){
+        this.name=name;
+        this.age=age;
+        this.id=id;
     }
 }
-arr.forEach((item,i)=>{
-    callback(item)
-})
+const u=new User("liumouyan",100,10)
